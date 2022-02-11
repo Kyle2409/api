de2c138659f0be04bf736cc099ea5495f1b32131
Kyle Mc Bryne <kylemcb.24@gmail.com>
2022-02-11 13:12:12 +0200

    fix process env variables

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const app = express.Router();
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-app.post('/',(req, res) => {
-    let{name,email,message} = req.body;
-    console.log(process.env.EMAIL, process.env.PASS)
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASS,
-        }
-      });
-      
-      const mailOptions = {
-        from: email,
-        to: 'kylemcb.24@gmail.com',
-        subject: 'New Contact from portfolio',
-        text: `${name} has contacted you
+
+const app = express.Router();
+
+app.post("/", (req, res) => {
+  let { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "kylemcb.24@gmail.com",
+    subject: "New Contact From Portfolio",
+    text: `${name} has contacted you 
         
         Please contact them back on ${email}
+        
+        ${message}`,
+  };
 
-        ${message}
-        `,
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-          res.status(400).send({msg: "Email not sent" + error});
-        } else {
-          console.log('Email sent: ' + info.response);
-          res.send({msg:"Email has been sent successfully"});
-        }
-      });
-    })
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(404).send({ msg: "Email not sent " + error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Email sent successfully!" });
+    }
+  });
+});
 module.exports = app
